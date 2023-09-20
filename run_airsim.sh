5edaa6ed129529b23a0f0360b26dd8383069821e
gus-robotics88 <142263265+gus-robotics88@users.noreply.github.com>
2023-09-20 16:21:49 -0700

    Removing unnecessary mavproxy out arg (#4)

--- run_airsim.sh
+++ run_airsim.sh
@@ -1,4 +1,4 @@
 sim_vehicle.py -v ArduCopter -f airsim-copter \
     --add-param-file=libraries/SITL/examples/Airsim/lidar.parm \
     --add-param-file=r88_airsim_params.parm \
-    -m "--out=udp:127.0.0.1:14551 --streamrate=-1" \
+    -m --streamrate=-1 \
