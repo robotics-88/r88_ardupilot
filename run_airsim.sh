5d37afcdae576a58a6a904ee8342ae799ae20908
Gus Meyer <142263265+gus-robotics88@users.noreply.github.com>
2023-12-09 14:02:12 -0800

    Making OA run at 10 hz and setting relevant params for sim (#9)

--- run_airsim.sh
+++ run_airsim.sh
@@ -1,4 +1,3 @@
 sim_vehicle.py -v ArduCopter -f airsim-copter \
-    --add-param-file=libraries/SITL/examples/Airsim/lidar.parm \
     --add-param-file=r88_airsim_params.parm \
     -m --streamrate=-1 \
